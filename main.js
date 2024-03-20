d086f7337861e553754e9f3d53f373839c65b1a7
Adongo Jakes <116814115+Adong-o@users.noreply.github.com>
2024-03-20 14:31:18 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,43 +1,52 @@
-const form = document.querySelector('.top-banner form');
-const input = document.querySelector('.top-banner input');
-const msg = document.querySelector('.top-banner .msg');
-const list = document.querySelector('.top-banner .cities');
-
+/*SEARCH BY USING A CITY NAME (e.g. athens) OR A COMMA-SEPARATED CITY NAME ALONG WITH THE COUNTRY CODE (e.g. athens,gr)*/
+const form = document.querySelector(".top-banner form");
+const input = document.querySelector(".top-banner input");
+const msg = document.querySelector(".top-banner .msg");
+const list = document.querySelector(".ajax-section .cities");
+/*SUBSCRIBE HERE FOR API KEY: https://home.openweathermap.org/users/sign_up*/
 const apiKey = "4d8fb5b93d4af21d66a2948710284366";
 
 form.addEventListener("submit", e => {
   e.preventDefault();
   let inputVal = input.value;
 
-const listItems = list.querySelectorAll('.ajax-section .city');
-const listItemsArray = Array.from(listItems);
+  //check if there's already a city
+  const listItems = list.querySelectorAll(".ajax-section .city");
+  const listItemsArray = Array.from(listItems);
 
-if (listItemsArray.length > 0){
+  if (listItemsArray.length > 0) {
     const filteredArray = listItemsArray.filter(el => {
-        let content = "";
-        //Athens,gr
-        if(inputVal.split(",")){
-            if(inputVal.split(",")[1].length > 2){
-                inputVal = inputVal.split(",")[0];
-                content = el
-                .querySelector(".city-name span")
-                .textContent.toLocaleLowerCase();
-            } else {
-                content = el.querySelector(".city-name").CDATA_SECTION_NODE.name.toLocaleLowerCase();
-            }
-            return content == inputVal.toLocaleLowerCase()
+      let content = "";
+      //athens,gr
+      if (inputVal.includes(",")) {
+        //athens,grrrrrr->invalid country code, so we keep only the first part of inputVal
+        if (inputVal.split(",")[1].length > 2) {
+          inputVal = inputVal.split(",")[0];
+          content = el
+            .querySelector(".city-name span")
+            .textContent.toLowerCase();
+        } else {
+          content = el.querySelector(".city-name").dataset.name.toLowerCase();
         }
+      } else {
+        //athens
+        content = el.querySelector(".city-name span").textContent.toLowerCase();
+      }
+      return content == inputVal.toLowerCase();
     });
-    if(filteredArray.length > 0){
-        msg.textContent = `You already know the weather for ${filteredArray[0].querySelector(".city-name span").textContent} ...otherwise be more specific by providing the country code as well🥱`;
-        form.reset();
-        input.focus();
-        return;
-    }
-}
 
-const url = `https://api.openweathermap.org/data/2.5/weather?q=${inputVal}&appid=${apiKey}&units=metric`;
-//const url = https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}
+    if (filteredArray.length > 0) {
+      msg.textContent = `You already know the weather for ${
+        filteredArray[0].querySelector(".city-name span").textContent
+      } ...otherwise be more specific by providing the country code as well 😉`;
+      form.reset();
+      input.focus();
+      return;
+    }
+  }
+
+  //ajax here
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${inputVal}&appid=${apiKey}&units=metric`;
 
   fetch(url)
     .then(response => response.json())
